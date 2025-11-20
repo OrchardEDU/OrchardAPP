cf703f52925c6bc952fb1949e2c9214c530bb412
Ethan <1bluejellybean@gmail.com>
2025-11-20 15:50:17 -0500

    remove faulty login and demo button

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -16,7 +16,6 @@ const Footer = () => {
                         <div className="link-group">
                             <h4>Product</h4>
                             <a href="#features">Features</a>
-                            <a href="#">Demo</a>
                         </div>
                         <div className="link-group">
                             <h4>Company</h4>
